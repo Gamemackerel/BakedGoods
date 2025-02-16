6f46b3693a0d4dea168cdc2ecc38e70734da0b70
Abraham Miller <abraham.r.m@gmail.com>
2025-02-16 11:36:28 -0800

    Add github link

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { Link } from 'lucide-react'
 import BakedGoodsGame from './components/BakedGoodsGame'
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <BakedGoodsGame />
+      <footer className="absolute bottom-2 left-1/2 transform -translate-x-1/2 text-sm text-gray-500">
+        <a className='hover:underline' href='https://github.com/Gamemackerel/BakedGoods'>View on GitHub</a>
+      </footer>
     </main>
   )
 }
--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -4,6 +4,9 @@ export default function StatsPage() {
     return (
         <main className="min-h-screen">
           <StatsView />
+          <footer className="absolute bottom-2 left-1/2 transform -translate-x-1/2 text-sm text-gray-500">
+            <a className='hover:underline' href='https://github.com/Gamemackerel/BakedGoods'>View on GitHub</a>
+          </footer>
         </main>
       )
   }
