abd7563b7a9f6eb10b10d1657b7634a5a0c4c05b
Abraham Miller <abraham.r.m@gmail.com>
2025-02-16 16:38:39 -0800

    fix footer

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import BakedGoodsGame from './components/BakedGoodsGame'
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
+    <main>
       <BakedGoodsGame />
-      <footer className="absolute bottom-2 left-1/2 transform -translate-x-1/2 text-sm text-gray-500">
+      <footer className="absolute bottom-2 left-1/2 transform -translate-x-1/2 text-sm text-gray-500 z-[-1]">
         <a className='hover:underline' href='https://github.com/Gamemackerel/BakedGoods'>View on GitHub</a>
       </footer>
     </main>
